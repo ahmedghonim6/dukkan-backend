715855ceca91098574d94b1b11c4a59720656365
Ahmed <ahmed@test.com>
2026-05-10 18:30:00 +0300

    remove prisma config

--- prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  datasources: {
-    db: {
-      url: "postgresql://postgres.ejzamefhuhflruozakji:WyRvmU*5nvTp6Z-@aws-1-eu-north-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
-    }
-  }
-}
