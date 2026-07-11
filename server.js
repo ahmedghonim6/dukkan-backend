257df9b47a704ab1c6115a9622327da4effc4ebb
Ahmed <ahmed@test.com>
2026-07-11 23:30:41 +0300

    allow all origins for cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json({ strict: false }))
 app.use(express.urlencoded({ extended: true }))
 app.use((req,res,next)=>{console.log('Body:',req.body);next()})
